7877f225896cf67114246e8a882695b8b7648e68
mjy3966@gmail.com <mjy3966@gmail.com>
2018-11-23 01:03:13 -0800

    first_commit

---
+++ topic_first/src/simple_sub.cpp
@@ -0,0 +1,16 @@
+#include "ros/ros.h"
+#include "topic_first/simplemsg.h"
+void chatterCallerback(const topic_first::simplemsgConstPtr& ptr)
+{
+    ROS_INFO("I heard from %s : [%s]", ptr->id.c_str(), ptr->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "simple_sub");
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallerback);
+    ros::spin();
+    return 0;
+}
